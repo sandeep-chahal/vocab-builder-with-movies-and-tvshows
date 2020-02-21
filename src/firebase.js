af5d98f90323e408026fdf5b008caf0a802ec3c4
sandeep-chahal <sandeepchahal573@gmail.com>
2020-02-21 15:44:43 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+var firebaseConfig = {
+	apiKey: "AIzaSyAyvH2rV74bLhE3CIpKgj_pvdObDam7eOE",
+	authDomain: "vocab-builder-b715d.firebaseapp.com",
+	databaseURL: "https://vocab-builder-b715d.firebaseio.com",
+	projectId: "vocab-builder-b715d",
+	storageBucket: "vocab-builder-b715d.appspot.com",
+	messagingSenderId: "1036252159325",
+	appId: "1:1036252159325:web:3da21d81b6e8fe5f251cb4",
+	measurementId: "G-D4Y0V6M98N"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase;
